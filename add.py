99b4310ca0ebcfb58717907b2c95d033eabef331
Matheus Ribeiro <matheusvlribeiro@gmail.com>
2023-08-21 10:33:35 -0300

    Criado arquivo de carregamento de dados e bla bla bla l

--- add.py
+++ add.py
@@ -1,15 +1,11 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
+from src.extraction import load_data
 
+st.set_page_config(layout = 'wide')
 
-def load_data():
-    return pd.read_csv('data/processed/bikes_compiled.csv')
-
-
-def main()
-    df = load_data()
-    st.dataframe(df)
+def main():
+    df_raw = load_data()
+    st.dataframe(df_raw)
 
 if __name__ == '__main__':
     main()
---
+++ src/extraction.py
@@ -0,0 +1,3 @@
+import pandas as pd
+def load_data():
+    return pd.read_csv('data/processed/bikes_compiled.csv')
