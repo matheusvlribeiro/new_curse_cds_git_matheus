5f63958065e36f709986b802aad3a8f077507867
Matheus Ribeiro <matheusvlribeiro@gmail.com>
2023-08-24 13:50:58 -0300

    Adicionado secao de descricao de dados

--- add.py
+++ add.py
@@ -3,9 +3,16 @@ from src.extraction import load_data
 
 st.set_page_config(layout = 'wide')
 
+def create_dataframe_section(df):
+    st.title('Database Section')
+    col1, col2 = st.columns(2)
+
+
 def main():
     df_raw = load_data()
+    create_dataframe_section(df_raw)
     st.dataframe(df_raw)
+    
 
 
 
